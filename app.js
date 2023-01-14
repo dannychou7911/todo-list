1278021dc2b13e7b3f4f34fe09bc2038eb7223a0
DannyChou <ericx790101@gmail.com>
2023-01-14 23:43:46 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
@@ -6,12 +7,21 @@ const methodOverride = require('method-override');
 const routes = require('./routes');
 
 // Mongoose 連線設定只需要「被執行」，不需要接到任何回傳參數繼續利用，所以這裡不需要再設定變數。
-require('./config/mongoose')
+require('./config/mongoose');
 
 const app = express();
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
+
+// session
+app.use(
+    session({
+        secret: 'ThisIsMySecret',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }));
